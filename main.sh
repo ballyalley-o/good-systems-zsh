31af02898a92982e5c60d76a1e2042d65bde65d6
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-04 11:23:08 +1300

    🦄 refactor(main for custom commands):

---
+++ main.sh
@@ -0,0 +1,19 @@
+# colors
+source ~/mac-zshrc/utilities/colors.sh
+# loading bar
+source ~/mac-zshrc/utilities/loading-bar.sh
+# sub-functions
+source ~/mac-zshrc/utilities/functions.sh
+# aliases
+source ~/mac-zshrc/utilities/aliases.sh
+# formatting
+source ~/mac-zshrc/utilities/formatting.sh
+# logging
+source ~/mac-zshrc/utilities/logging.sh
+# workspace
+source ~/mac-zshrc/workspace/workspace.sh
+# howick
+source ~/mac-zshrc/howick/hltd.sh
+# iod
+source ~/mac-zshrc/iod/helpers.sh
+source ~/mac-zshrc/iod/iod.sh
