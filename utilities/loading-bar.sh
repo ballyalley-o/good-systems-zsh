1715db5e9c7ea413bc13afe6ac00f92f47c8ef3f
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-03 18:52:16 +1300

    🦄 refactor(loading bar):

---
+++ utilities/loading-bar.sh
@@ -0,0 +1,29 @@
+source ~/mac-zshrc/utilities/colors.sh
+
+# loading bar
+# Usage: loading_bar <duration: 0.2> <color: YELLOW> <message: Searching:>
+loading_bar() {
+    local width=20
+    local duration=${1:-0.2}
+    local i
+
+    color=${2:-"$YELLOW"}
+    msg=${3:-Searching:}
+
+    echo -n -e "$msg"
+
+    while true; do
+        sleep "$duration"
+        echo -n -e "${color}█${RESET}"
+        ((width--))
+
+        if [ "$width" -eq 0 ]; then
+            break
+        fi
+    done
+
+    tput cuu1
+
+    echo
+    echo "Loading complete                      "
+}
