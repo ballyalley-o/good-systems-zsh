2b6dddefc7954389fa4e10d8e55cb9f3fac3d1a4
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-05 21:18:08 +1300

    sample students added

---
+++ iod/sample-students.sh
@@ -0,0 +1,4 @@
+declare -a students=("claire" "bob" "john")
+declare -a students_lastname=("claire smith" "bob ong" "john hurray")
+declare -a students_email=("claire@test.com" "bob@email.com" "hurray.john@gmail.com")
+declare -a students_usernames=("@claire123" "@bobOng" "@hurrayJ")
