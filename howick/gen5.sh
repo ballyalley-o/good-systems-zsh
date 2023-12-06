b618c88cb0515a2323989d528ccd26282dab64e9
Jasper <jasperjian@outlook.co.nz>
2023-12-06 14:49:14 +1300

    🐎 ci(added ini open):

--- howick/gen5.sh
+++ howick/gen5.sh
@@ -1,7 +1,28 @@
+source ~/win-bashrc/mac-zshrc/utilities/colors.sh
+
+ini_path=$HOME/AppData/Roaming/HowickHLCv3
+ini_file=Howick.ini
+
 gen5() {
      case "$1" in
 	 main)
-	     cd ~/Documents/HOWICK-repos/hmi-gen-5-official/HLCv3 && code .
+		case "$2" in
+
+	       	.)
+		 		cd ~/Documents/HOWICK-repos/hmi-gen-5-official/HLCv3 && code .
+				;;
+			-i)
+				cd $ini_path && start $ini_file && cat $ini_file | awk "NR==3"
+				echo -e "${INVERTED} $ini_file from $ini_path Opened ${RESET}"
+				;;
+	       *)
+				echo "Usage: gen5 main [ . | -i ]"
+				echo
+				echo "gen5 main . 		Navigate to the howick gen5 main local and open VS Code"
+				echo "gen5 main -i		Navigate to the HowickHLCv3 & Open the machine configuration (ini)"
+				return 1
+				;;
+            esac
 	     ;;
 	 logis)
 	    cd ~/Documents/HOWICK-repos/finish_profiles/HLCv3 && code  .
@@ -16,6 +37,9 @@ gen5() {
 		;;
 	       *)
 		echo "Usage: gen5 repos [-1 | -2]"
+		echo
+		echo "gen5 repos -1 	Open the terminal in howick local repositories"
+		echo "gen5 repos -2		Open the terminal in howick local repositories and Show the folder"
 		return 1
 		;;
             esac
