48341ded895e6483e7d969d8d1cedfdd5a2583b0
Jasper <jasperjian@outlook.co.nz>
2023-12-06 16:20:43 +1300

    moved add profile to gen5

--- howick/gen5.sh
+++ howick/gen5.sh
@@ -44,6 +44,39 @@ gen5() {
 		;;
             esac
 	    ;;
+
+		 # TODO: add a command that adds a profile
+
+                #!/bin/bash
+
+                # ini_file="example.ini"
+                # search_key="key2"
+                # new_key="key5"
+                # new_value="new_value5"
+
+                # awk -v search_key="$search_key" -v new_key="$new_key" -v new_value="$new_value" '
+                # BEGIN {
+                #     FS=" *= *"        # Set the field separator to handle spaces around the equal sign
+                #     OFS=" = "         # Set the output field separator
+                #     section_found=0   # Flag to track if the section has been found
+                # }
+                # {
+                #     if ($0 ~ /^\[/) {   # Check if the line starts with '[' indicating a section
+                #         section_found=0
+                #     }
+                #     if (section_found && $1 == search_key) {
+                #         print $0           # Print the original line
+                #         print new_key, new_value   # Append the new key-value pair
+                #         section_found=0     # Reset the flag as we have processed the section
+                #     } else {
+                #         print $0           # Print the original line
+                #     }
+                # }
+                # $1 == "[" section_name "]" {
+                #     section_found=1   # Set the flag when the desired section is found
+                # }
+                # ' "$ini_file" > temp_ini && mv temp_ini "$ini_file"
+
 	 *)
 	    echo "Usage: gen5 [main | logis | repos] [-1 | -2]"
 	    return 1
