9f2da31921bf19b23153cfe1b2a0cb85a2c11564
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-21 08:34:07 +1300

    made it compatible with bash

--- utilities/font-switch.sh
+++ utilities/font-switch.sh
@@ -6,10 +6,10 @@ fontsw() {
     if grep -q '"editor.fontFamily": "JetBrainsMono-Bold"' "$settings_file"; then
         loading_bar 0.01 ${BLUE} "SWITCHING FONT":
         tput cuu1
-        sed -i '' 's/"editor.fontFamily": "JetBrainsMono-Bold"/"editor.fontFamily": "JetBrains Mono"/' "$settings_file"
+        sed -i.bak 's/"editor.fontFamily": "JetBrainsMono-Bold"/"editor.fontFamily": "JetBrains Mono"/' "$settings_file"
         echo -e "${INVERTED} ☡ FONT Changed to JetBrains Mono ${RESET}"
     else
-        sed -i '' 's/"editor.fontFamily": "JetBrains Mono"/"editor.fontFamily": "JetBrainsMono-Bold"/' "$settings_file"
+        sed -i.bak 's/"editor.fontFamily": "JetBrains Mono"/"editor.fontFamily": "JetBrainsMono-Bold"/' "$settings_file"
         loading_bar 0.01 ${MAGENTA} "SWITCHING FONT":
         tput cuu1
         echo -e "${MAGENTABG} ☡ FONT Changed to JetBrainsMono-Bold ${RESET}"
