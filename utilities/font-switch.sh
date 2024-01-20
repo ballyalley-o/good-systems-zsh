a16174def8e7610a6c54554d126db375b9b64510
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-20 19:52:46 +1300

    📃 docs(Update font-switch.sh to switch between JetBrains Mono and JetBrainsMono-Bold):

--- utilities/font-switch.sh
+++ utilities/font-switch.sh
@@ -1,14 +1,18 @@
-source ~/mac-zshrc/utilities/colors.sh
-
+# Function to switch the font in Visual Studio Code settings
+# Arguments: None
+# Returns: None
 fontsw() {
     settings_file="$HOME/Library/Application Support/Code/User/settings.json"
 
+    # Check if the current font is JetBrainsMono-Bold
     if grep -q '"editor.fontFamily": "JetBrainsMono-Bold"' "$settings_file"; then
         loading_bar 0.01 ${BLUE} "SWITCHING FONT":
         tput cuu1
+        # Replace the font with JetBrains Mono
         sed -i '' 's/"editor.fontFamily": "JetBrainsMono-Bold"/"editor.fontFamily": "JetBrains Mono"/' "$settings_file"
         echo -e "${INVERTED} ☡ FONT Changed to JetBrains Mono ${RESET}"
     else
+        # Replace the font with JetBrainsMono-Bold
         sed -i '' 's/"editor.fontFamily": "JetBrains Mono"/"editor.fontFamily": "JetBrainsMono-Bold"/' "$settings_file"
         loading_bar 0.01 ${MAGENTA} "SWITCHING FONT":
         tput cuu1
