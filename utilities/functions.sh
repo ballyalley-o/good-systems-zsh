ac4b0ca4bc8a44d8e48f365e9893c578afe25500
bally <bally.lomibao@howickltd.com>
2024-03-04 10:39:10 +1300

    Add gitl function to display git log with specified number of commits

--- utilities/functions.sh
+++ utilities/functions.sh
@@ -8,3 +8,8 @@ function gitn
 {
     git log --oneline --abbrev-commit --all --graph --decorate --color -n 5
 }
+
+function gitl
+{
+    git log --oneline --abbrev-commit --all --graph --decorate --color -n $1
+}
